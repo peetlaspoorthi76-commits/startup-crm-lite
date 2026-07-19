d60abb03619665d2179892126b58df47dc5bf062
Spoorthi <peetlaspoorthi76@gmail.com>
2026-07-19 17:20:48 +0530

    Add Toaster back to see auth errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import AppRoutes from './routes'; // Ensure this points to your index.jsx correctly
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <AppRoutes />
     </BrowserRouter>
   );
